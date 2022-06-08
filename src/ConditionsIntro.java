8f7777a65184674d608a0ed0c2a9ca6e33c23d22
Valentin Payet <valentinpayet06@gmail.com>
2022-06-08 15:29:30 +0200

    Coursjava8juin

--- src/ConditionsIntro.java
+++ src/ConditionsIntro.java
@@ -1,2 +1,32 @@
-package PACKAGE_NAME;public class ConditionsIntro {
+public class ConditionsIntro {
+    public static void main(String[] args) {
+        // Declaration de la variable age
+        byte age=18;
+
+        //Test condition
+        //si age >= 18 alors
+        // Afficher "Vous êtes majeur, et pouvez donc participer a la compétition"
+        // fin sin
+        //Sinon
+        //Afficher "Vous êtes mineur, et vous ne pouvez pas y participer"
+        //Fin sin
+        if(age>=18){
+            System.out.println("Vous etes majeur. Donc vous pouvez participer");
+        }else {
+            System.out.println("Vous n'etes pas majeur, donc vous ne pouvez pas participer");
+        }
+
+        String pays="Chines";
+        if(pays=="France"){
+            System.out.println("Bonjour");
+        } else if (pays=="Allemagne") {
+            System.out.println("Guten tag");
+        } else if (pays=="Espagne") {
+            System.out.println("Hola");
+        } else if (pays=="Chine") {
+            System.out.println("Ninhau");
+        }else{
+            System.out.println("LE pays fourni n'est pas supporte");
+        }
+    }
 }
---
+++ src/Variable.java
@@ -0,0 +1,36 @@
+public class Variable {
+    public static void main(String[] args) {
+        //Les variables/fonctions en Java sont en camelCase
+        //Les classes vont être enn PascalCase
+
+        //Les entiers
+
+        byte age=18;//La variable ne peut stocker que des entiers
+        //Calcul Rectangle
+        int longueur=50;
+        int largeur=25;
+        int surface=longueur*largeur;
+        int perimetre=2*(longueur+largeur);
+        System.out.println(surface);
+        System.out.print(perimetre);
+
+        // Reels
+        //Calcul de la vitesse moyenne en MRU
+        float distanceParcours=90.6F;
+        float temps=12.345F;
+        double vitesseMoyenne=distanceParcours/temps;
+        double vitesseMoyenne2=56.45;
+
+        //Calcul somme des nombres
+        float prix=150.5F;
+
+        // Chaine de caracteres et caracters
+        String prenom="Christian";// Chaine de caractères
+        char genre='M';
+
+
+        //35.7654567876543 : double
+        //35.6 : float
+
+    }
+}
